4c6039cfb9a28453f9de5946f9366b75d738e62b
Wen <wfwuestc@163.com>
2017-10-08 16:16:30 +0800

    delete leancloud test

--- src/js/App.js
+++ src/js/App.js
@@ -15,13 +15,14 @@ AV.init({
   appKey: APP_KEY
 });
 
-var TestObject = AV.Object.extend('TestObject');
-var testObject = new TestObject();
-testObject.save({
-  words: 'Hello World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
-})
+// var TestObject = AV.Object.extend('TestObject');
+// var testObject = new TestObject();
+// testObject.save({
+//   words: 'Hello Wen!',
+//   test: 'hi fengwei'
+// }).then(function(object) {
+//   alert('LeanCloud Rocks!');
+// })
 
 
 var log = console.log.bind(console)
