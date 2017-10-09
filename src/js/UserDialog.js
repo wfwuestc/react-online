7b0c2ff4ae61aa00809cba8aefa17613dab2fbe8
Wen <wfwuestc@163.com>
2017-10-09 12:13:28 +0800

    不直接修改state

--- src/js/UserDialog.js
+++ src/js/UserDialog.js
@@ -61,7 +61,7 @@ export default class UserDialog extends Component {
         </form>
     )
     let signInForm = (
-        <form className="signIn" onSubmit={this.signUp.bind(this)}> {/* 注册*/}
+        <form className="signIn" onSubmit={this.signIn.bind(this)}> {/* 注册*/}
           <div className="row">
             <label>用户名</label>
             <input type="text" value={this.state.formData.username}
