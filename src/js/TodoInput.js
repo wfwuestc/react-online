05e9864fbbdc0184fcaf7a1b184682fac54052f9
Wen <wfwuestc@163.com>
2017-10-11 10:17:16 +0800

    class -> function

--- src/js/TodoInput.js
+++ src/js/TodoInput.js
@@ -1,21 +1,39 @@
 import React, {Component} from 'react'
 import '../css/TodoInput.css'
 
-export default class TodoInput extends Component {
-  render() {
-    return <input type="text" value={this.props.content}
-                  className="TodoInput"
-                  onKeyPress={this.submit.bind(this)} onChange={this.changeTitle.bind(this)}/>
-  }
+// export default class TodoInput extends Component {
+//   render() {
+//     return <input type="text" value={this.props.content}
+//                   className="TodoInput"
+//                   onKeyPress={this.submit.bind(this)} onChange={this.changeTitle.bind(this)}/>
+//   }
+//
+//   submit(e) {
+//     if (e.key === 'Enter') {
+//       console.log('按回车了')
+//       this.props.onSubmit(e)
+//     }
+//   }
+//
+//   changeTitle(e) {
+//     this.props.onChange(e)
+//   }
+// }
 
-  submit(e) {
+function submit (props, e) {
     if (e.key === 'Enter') {
-      console.log('按回车了')
-      this.props.onSubmit(e)
-    }
+      if (e.target.value.trim() !== '') {
+              props.onSubmit(e)
+            }
   }
+}
+function changeTitle (props, e) {
+  props.onChange(e)
+}
 
-  changeTitle(e) {
-    this.props.onChange(e)
-  }
+export default function (props) {
+  return <input type="text" value={props.content}
+                className="TodoInput"
+                onChange={changeTitle.bind(null, props)}
+                onKeyPress={submit.bind(null, props)}/>
 }
